af33fc68691f407cf1faa53187a4cdae8a14d8b2
Pat Meyers <pat.meyers@unimelb.edu.au>
2021-05-05 18:16:54 +1000

    update name in init file

--- baboo/__init__.py
+++ baboo/__init__.py
@@ -1,5 +1,5 @@
-"""Top-level package for baboo."""
+"""Top-level package for state_space_pulsar_inference."""
 
-__author__ = """Patrick Meyers"""
+__author__ = """Pat Meyers"""
 __email__ = 'pat.meyers@unimelb.edu.au'
 __version__ = '0.1.0'
