41705035a840b7f4a41d3d4bb3acb014133f211f
Liam Dunn <liamd@student.unimelb.edu.au>
2024-01-19 15:20:58 +1100

    Clean up simulate script some more, save new par file at the end

--- baboo/baboo_simulate_toas_onecomp_from_par_tim.py
+++ baboo/baboo_simulate_toas_onecomp_from_par_tim.py
@@ -7,7 +7,7 @@ import argparse
 import random
 from scipy.optimize import minimize
 from utils import fit_toas_to_get_frequencies, write_par, write_tim_file, write_freqs_fit_file
-from simulation import OneComponentModelSim
+from simulation import OneComponentF2NoiseModelSim
 import tempfile
 
 def run(params):
@@ -34,20 +34,20 @@ def run(params):
     Textrap = (pets0 - newpsr['PEPOCH'].val) * 86400
 
     # omega(t_0) = F(pepoch) + Fdot(pepoch) * (t_0 - PEPOCH) + Fddot(pepoch)*(t_0 - PEPOCH)^2 / 2.
-    omgc0 = newpsr['F0'].val + newpsr['F1'].val * Textrap + params.omgc_ddot * 0.5 * Textrap**2
-    omgc_dot = newpsr['F1'].val + params.omgc_ddot * Textrap
+    omgc0 = newpsr['F0'].val + newpsr['F1'].val * Textrap + params.fdd * 0.5 * Textrap**2
+    omgc_dot = newpsr['F1'].val + params.fdd * Textrap
     omgc_pepoch = newpsr['F0'].val
 
     write_par(params.output_tag, newpsr['F0'].val,
-            newpsr['F1'].val, PEPOCH,
+            newpsr['F1'].val, PEPOCH, F2=params.fdd,
               F0err=1e-3, F1err=np.abs(omgc_dot/3))
 
     # values at start time
 
     phi0 = random.random()
-    p0 = np.asarray([phi0, omgc0, omgc_dot])
+    p0 = np.asarray([phi0, omgc0, omgc_dot, params.fdd])
 
-    model_class = OneComponentModelSim(F2=params.omgc_ddot, Q_f0=params.xi_phase**2, Q_f1=params.xi_freq**2)
+    model_class = OneComponentF2NoiseModelSim(Q_phi=params.xi_phase**2, Q_f0=params.xi_freq**2, Q_f1=params.xi_fdot**2, Q_f2=params.xi_fddot**2)
     toas, toa_errors, states, pn = model_class(pets, p0, toa_errors)
 
     tmpdir = tempfile.TemporaryDirectory()
@@ -87,7 +87,7 @@ def run(params):
     residuals = psr.residuals()
 
     # save output par
-    #psr.savepar(params.output_tag + '.par')
+    psr.savepar(params.output_tag + '.par')
 
     # get libstempo fit residuals
     plt.errorbar(toas, residuals * omgc0, yerr=toa_errors * omgc0, fmt='x', label='libstempo')
@@ -98,12 +98,12 @@ def run(params):
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--xi_freq", default=1e-11, type=float, help='white noise in frequrency derivative')
+    parser.add_argument("--xi_freq", default=0, type=float, help='white noise in frequency derivative')
     parser.add_argument("--xi_phase", default=0, type=float, help='white noise in phase derivatives')
     parser.add_argument("--xi_fdot", default=0, type=float, help='white noise in second derivative of frequency')
     parser.add_argument("--xi_fddot", default=0, type=float, help='white noise in third derivative of frequency')
 
-    parser.add_argument("--omgc-ddot", default=0, type=float, help="starting second derivative")
+    parser.add_argument("--fdd", default=0, type=float, help="starting second derivative")
     parser.add_argument("--output-tag", default='out', type=str, help='output file tag')
     parser.add_argument("--tim-file", default=None, type=str, help="simulate based on observing cadence", required=True)
     parser.add_argument("--par-file", default=None, type=str, help="simulate based on observing cadence", required=True)
